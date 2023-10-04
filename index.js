a5362c1a9a8639ac35367235fb1b7694ff2807e9
Austin Byrd <byrda05@gmail.com>
2023-10-04 13:45:45 -0500

    update

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    const event = document.getElementById('text');
+
+    if(event) {
+        event.textContent = "This is really cool!";
+    }
+  });
